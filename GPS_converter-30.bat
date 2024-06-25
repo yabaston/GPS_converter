fe75c1398e51eb5c61ac43b11a951eea4537895f
yabaston <alexey.bmk@gmail.com>
2024-06-25 20:14:07 +0300

    Add example files

---
+++ GPS_converter-30.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Проверяем, был ли передан параметр
+if "%~1"=="" (
+    echo No file path specified.
+    exit /b 1
+)
+
+REM Устанавливаем переменную filepath равной первому параметру
+set "filepath=%~1"
+
+REM Выводим путь к файлу для проверки
+echo File path: "%filepath%"
+
+REM Запускаем ваше приложение с передачей пути к файлу в кавычках
+GPS_converter.exe "%filepath%" -30
+
+
+REM Завершаем скрипт
+exit /b 0
